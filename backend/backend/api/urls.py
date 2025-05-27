598f36285c6d9eee1e322f00e5e5f268923c1fe9
Franck andritina <rakotomavofranck007@gmail.com>
2025-05-27 15:11:32 +0300

    this is the frontend of my portfolio using react

---
+++ backend/backend/api/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from base.api.urls import  base_router
+
+router = DefaultRouter()
+
+router.registry.extend(base_router.registry)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
---
+++ backend/base/api/views.py
@@ -0,0 +1,10 @@
+from rest_framework.viewsets import ModelViewSet
+from base.models import Post
+from .serializers import PostSerializer
+
+class PostViewSet(ModelViewSet):
+    """
+    A viewset for viewing and editing post instances.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
